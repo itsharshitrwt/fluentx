138eb017fdb70304270e2cd0f6abe6de9c7568b6
itsharshitrwt <harshit.rawat1804@gmail.com>
2025-01-11 01:10:29 +0530

    initials

---
+++ src/app/components/Navbaar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbaar = () =>{
+    return(
+        <div className="bg-purple-600 h-[5vh] md:h-[10vh]">Navbaar</div>
+    )
+}
+
+export default Navbaar
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,48 @@
+'use client'
 import Image from "next/image";
-
+import React , {useState, ChangeEvent} from "react"; 
+import TextArea from "./components/inputs/TextArea";
+import Navbaar from "./components/Navbaar"
 export default function Home() {
+  const [sourceText, setSourceText] = useState<string>("");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div >
+      <Navbaar/>
+      <div className="h-full w-full dark:bg-black dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+      <div className="">
+        <div className="relative overflow-hidden h-screen">
+             <div className="max-w-[80rem] mx-auto px-4 sm:px-6 py-10 sm:py-24 z-10">
+                <div className="text-center">
+                    <h1 className="text-6xl sm:text-6xl font-bold text-neutral-200 z-10">Fluent<span className="text-purple-600">X</span></h1>
+                    <h4 className="text-neutral-300 mt-5 z-10">Understanding Every Word, Including PDFs</h4>
+                    <div className="mt-7 sm:mt-12 mx-auto max-w-3xl relative">
+                      <div className="grid gap-4 md:grid-cols-2 grid-cols-1">
+                        <div className="relative z-10 felx flex-col space-x-3 border rounded-lg shadow-lg bg-neutral-900 border-purple-700 shadow-gray-900/20">
+                          <TextArea
+                          id="source-language"
+                          placeholder={'Source Language'}
+                          value={sourceText}
+                          onChange={(e: ChangeEvent<HTMLTextAreaElement>)=>{
+                            setSourceText(e.target.value);
+                          }}
+                          />
+                          <div className="flex flex-row justify-between w-full">
+                            <span className="cursor-pointer flex space-x-2 flex-row">
+                              
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+             </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+        
+    </div>  
+       
     </div>
   );
 }
+
+
